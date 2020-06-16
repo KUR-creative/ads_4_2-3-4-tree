1bcef95bd758df0a63d5fc61921412c1f6048474
kur <rhdnfka94@gmail.com>
2020-06-16 16:15:05 +0900

    Add node rep(resentation)

--- btree.py
+++ btree.py
@@ -5,10 +5,22 @@ Node = namedtuple(
     'Node',
     'is_leaf min_n max_n keys children', defaults=[(), ()])
 
-def Node3(is_leaf, *args): # 3 means max num of keys in node
-    return Node(is_leaf, 1, 3, *args) # 2-3-4
-def Node2(is_leaf, *args): # 2 means max num of keys in node
-    return Node(is_leaf, 1, 2, *args) # 2-3
+def rep(node):
+    return '[{}{}|{}|{}]'.format(
+        node.max_n, 'L' if node.is_leaf else 'M',
+        ' '.join(map(str, node.keys)),
+        ' '.join(str(c.keys[0]) + '.' for c in node.children)
+    ) if type(node) is Node else 'NotNode'
 
-root = Node3(True)
-print(root)
+def Node2(is_leaf, keys, children=()): # 2 = max num of keys
+    return Node(is_leaf, 1, 2, keys, children) # 2-3
+def Node3(is_leaf, keys, children=()): # 3 = max num of keys
+    return Node(is_leaf, 1, 3, keys, children) # 2-3-4
+
+def split_child(unfull_parent, child_index):
+    pass
+
+print(rep(Node3(False, (10, 12, 20),
+                   (Node3(True, (11,)), Node3(True, (13,))))))
+print(type(Node3(False, (10, 12, 20),
+                   (Node3(True, (11,)), Node3(True, (13,))))))
--- btree_test.py
+++ btree_test.py
@@ -1,2 +1,15 @@
 from hypothesis import given, example
 from hypothesis import strategies as st
+
+from btree import *
+
+
+def test_split_manually():
+    full_child = Node3(True, (11,12,13))
+    unfull_parent = Node3(False, (10, 2), (full_child,))
+
+    actual = split_child(unfull_parent, 0)
+    expect = Node3(False, (10, 12, 20),
+                   (Node3(True, (11,)), Node3(True, (13,))))
+    assert actual == expect, f'{rep(actual)} != {rep(expect)}'
+        
