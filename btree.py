abcd931d56ac1d80050fd0c8fd9b1fcf24fdd746
kur <rhdnfka94@gmail.com>
2020-06-16 17:22:16 +0900

    Add is_invalid(node)

--- btree.py
+++ btree.py
@@ -13,12 +13,39 @@ def rep(node):
     ) if type(node) is Node else 'NotNode'
 
 def Node2(is_leaf, keys, children=()): # 2 = max num of keys
-    return Node(is_leaf, 1, 2, keys, children) # 2-3
+    return Node(is_leaf, 1, 2, tuple(keys), children) # 2-3
 def Node3(is_leaf, keys, children=()): # 3 = max num of keys
-    return Node(is_leaf, 1, 3, keys, children) # 2-3-4
+    return Node(is_leaf, 1, 3, tuple(keys), children) # 2-3-4
 
-def split_child(unfull_parent, child_index):
-    pass
+def is_invalid(node):
+    if type(node) is not Node:
+        return 'NotNode'
+    n = len(node.keys)
+    keys = node.keys
+    max_n = node.max_n
+    min_n = node.min_n
+    n_children = len(node.children)
+    ret = ''.join([
+        f'n = {n} > {max_n} = max_n \n' if n > max_n else '',
+        f'n = {n} < {min_n} = min_n \n' if n < min_n else '',
+        f'#children = {n_children} != {n + 1} = n + 1 \n'
+        if (not node.is_leaf) and n_children != n + 1 else '',
+        f'keys not sorted: {keys} \n' if any(
+            keys[i] > keys[i+1] for i in range(len(keys)-1)
+        ) else ''
+    ])
+    return ret # If valid, return '' (falsey value)
+
+def split_child(unfull_parent, child_idx):
+    child = unfull_parent.children[child_idx] # child is full.
+    min_n, max_n = child.min_n, child.max_n
+    up_idx = max_n // 2 # index of elem to go parent 
+    '''
+    l_child = Node(
+        child.is_leaf, min_n, max_n,
+        child.keys[:up_idx]
+    '''
+        
 
 print(rep(Node3(False, (10, 12, 20),
                    (Node3(True, (11,)), Node3(True, (13,))))))
--- btree_test.py
+++ btree_test.py
@@ -6,10 +6,15 @@ from btree import *
 
 def test_split_manually():
     full_child = Node3(True, (11,12,13))
-    unfull_parent = Node3(False, (10, 2), (full_child,))
+    unfull_parent = Node3(
+        False, (10,), (full_child, Node3(True, (20,))))
 
     actual = split_child(unfull_parent, 0)
-    expect = Node3(False, (10, 12, 20),
+    expect = Node3(False, (10,),
                    (Node3(True, (11,)), Node3(True, (13,))))
-    assert actual == expect, f'{rep(actual)} != {rep(expect)}'
+    assert not is_invalid(unfull_parent)
+    assert not is_invalid(full_child)
+    assert not is_invalid(actual)
+    assert not is_invalid(expect)
+    assert actual == expect, rep(actual)+' != '+rep(expect)
         
