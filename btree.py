caef863fd4b48995e14254c3699edf53dc059ba7
kur <rhdnfka94@gmail.com>
2020-06-17 13:50:06 +0900

    Add incomplete insert and its incomplete test

--- btree.py
+++ btree.py
@@ -1,58 +1,83 @@
+from bisect import bisect
 from collections import namedtuple
 
 
 Node = namedtuple(
-    'Node',
-    'is_leaf min_n max_n keys children', defaults=[(), ()])
+    'Node', 'is_leaf keys children', defaults=[()])
 
 def rep(node):
     return '[{}{}|{}|{}]'.format(
-        node.max_n, 'L' if node.is_leaf else 'M',
+        'L' if node.is_leaf else 'M',
         ' '.join(map(str, node.keys)),
         ' '.join(str(c.keys[0]) + '.' for c in node.children)
     ) if type(node) is Node else 'NotNode'
 
-def Node2(is_leaf, keys, children=()): # 2 = max num of keys
-    return Node(is_leaf, 1, 2, tuple(keys), children) # 2-3
-def Node3(is_leaf, keys, children=()): # 3 = max num of keys
-    return Node(is_leaf, 1, 3, tuple(keys), children) # 2-3-4
+# max_n = 2: 2-3
+# max_n = 3: 2-3-4
+def leaf(*keys):
+    return Node(True, tuple(keys), ())
 
-def is_invalid(node):
+def is_invalid(node, max_n, min_n=1):
     if type(node) is not Node:
         return 'NotNode'
     n = len(node.keys)
     keys = node.keys
-    max_n = node.max_n
-    min_n = node.min_n
     n_children = len(node.children)
     ret = ''.join([
         f'n = {n} > {max_n} = max_n \n' if n > max_n else '',
         f'n = {n} < {min_n} = min_n \n' if n < min_n else '',
+        
         f'#children = {n_children} != {n + 1} = n + 1 \n'
         if (not node.is_leaf) and n_children != n + 1 else '',
+        
+        f'invalid leaf - children: {node.children}'
+        if node.is_leaf and len(node.children) > 0 else '',
+        f'invalid non-leaf: no children: {node.children}'
+        if (not node.is_leaf) and len(node.children) == 0 else '',
+        
         f'keys not sorted: {keys} \n' if any(
             keys[i] > keys[i+1] for i in range(len(keys)-1)
         ) else ''
     ])
     return ret # If valid, return '' (falsey value)
 
-def split_child(unfull_parent, child_idx):
+def tuple_insert(xs, idx, y):
+    return xs[:idx] + (y,) + xs[idx:]
+
+def insert(tree, key, max_n):
+    node = tree
+    
+    idx = bisect(node.keys, key)
+    new_keys = tuple_insert(node.keys, idx, key)
+    
+    if len(node.keys) < max_n:
+        return node._replace(keys = new_keys)
+    else:
+        up_idx = 1 # (2-3, 2-3-4 just same as 1)
+        up_key = node.keys[up_idx]
+        return node._replace(
+            is_leaf=False,
+            keys=(up_key,),
+            children=(leaf(*new_keys[:up_idx]),
+                      leaf(*new_keys[up_idx + 1:])))
+
+def split_child(unfull_parent, child_idx, max_n, min_n=1):
     ''' 
     Because it support 2-3 tree, it is different to CLRS. 
     Add to child, and split. that's it.
     '''
     child = unfull_parent.children[child_idx] # child is full.
-    min_n, max_n = child.min_n, child.max_n
     
     up_idx = max_n // 2 # index of elem to go parent 
     up_key = child.keys[up_idx]
     
-    l_child = Node(
-        child.is_leaf, min_n, max_n,
-        child.keys[:up_idx],
+    #l_child = Node(
+        #child.is_leaf, min_n, max_n, child.keys[:up_idx],
         
 
-print(rep(Node3(False, (10, 12, 20),
-                   (Node3(True, (11,)), Node3(True, (13,))))))
-print(type(Node3(False, (10, 12, 20),
-                   (Node3(True, (11,)), Node3(True, (13,))))))
+'''
+print(rep(Node(False, (10, 12, 20),
+                   (Node(True, (11,)), Node(True, (13,))))))
+print(type(Node(False, (10, 12, 20),
+                   (Node(True, (11,)), Node(True, (13,))))))
+'''
--- btree_test.py
+++ btree_test.py
@@ -1,17 +1,58 @@
+import pytest
 from hypothesis import given, example
 from hypothesis import strategies as st
+
+import funcy as F
 
 from btree import *
 
 
-def test_split_manually():
-    full_child = Node3(True, (11,12,13))
-    unfull_parent = Node3(
-        False, (10,), (full_child, Node3(True, (20,))))
+def test_insert_no_split():
+    max_n = 2
+    assert insert(leaf(10), 20, 2) == Node(True, (10, 20), ())
+    assert insert(leaf(20), 10, 2) == Node(True, (10, 20), ())
+    
+    max_n = 3
+    tree = insert(leaf(10), 20, 3)
+    assert tree == Node(True, (10, 20), ())
+    tree = insert(tree, 0, 3)
+    assert tree == Node(True, (0, 10, 20), ())
 
-    actual = split_child(unfull_parent, 0)
-    expect = Node3(False, (10,),
-                   (Node3(True, (11,)), Node3(True, (13,))))
+def test_insert_split():
+    max_n = 2
+    ins2 = lambda *args: insert(*args, 2)
+    root = ins2(leaf(5), 6)
+    root = ins2(root, 7)
+    assert root == Node(False, (6,), (leaf(5), leaf(7)))
+    
+    max_n = 3
+    ins3 = lambda *args: insert(*args, 3)
+    #root = insert(leaf(5), 6, 2)
+    #root = insert(root, 7, 2)
+
+#@given(st.lists(st.integers(), min_size = 1))
+#def test_insert_prop_test(xs):
+def test_insert_prop_test():
+    # number of keys inserted/flattend btree are same
+    # all nodes are valid
+    
+    # keys are sorted from dfs
+    # all leaves have same height
+    tree = Node(False, (6,), (leaf(5), leaf(7)))
+    print(Node(False, (6,), (leaf(5), leaf(7))))
+    print(F.lflatten(tree))
+    assert False
+
+@pytest.mark.skip(reason="no way of currently testing this")
+def test_split_manually():
+    max_n = 3
+    full_child = Node(True, (11,12,13))
+    unfull_parent = Node(
+        False, (10,), (full_child, Node(True, (20,))))
+
+    actual = split_child(unfull_parent, 0,)
+    expect = Node(False, (10,),
+                   (Node(True, (11,)), Node(True, (13,))))
     assert not is_invalid(unfull_parent)
     assert not is_invalid(full_child)
     assert not is_invalid(actual)
